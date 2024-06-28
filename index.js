8089ea17187c44ecec6013639cef850d72676772
Maryam Dawood <mdawood832@gmail.com>
2024-06-28 16:35:54 -0500

    project wrapper changes

--- index.js
+++ index.js
@@ -2,32 +2,6 @@ let isModalOpen = false
 let contrastToggle=false
 const scaleFactor = 20
 
-function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape")
-    const x = event.clientX / scaleFactor
-    const y = event.clientY / scaleFactor
-    
-    for (let i=0 ; i<shapes.length ; i++ ) {
-        const isOdd = i % 2 !== 0 ; 
-        const boolInt = isOdd ? -1 : 1
-        shapes[i].style.transform = `translate(${x * boolInt}px , ${y * boolInt}px)`
-    }
-}
-
-
-
-
-function toggleContrast (){
-    contrastToggle= !contrastToggle
-
-    if(contrastToggle){
-        document.body.classList += "dark-theme"
-    }
-
-    else{
-        document.body.classList.remove("dark-theme")
-    }
-}
 
 function toggleModal() {
     if (isModalOpen){
@@ -58,6 +32,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
 
 
 
